3fcdb19fe65f9dddcf0175e9a9712981d601218c
David Grudl <david@grudl.com>
2018-09-18 11:47:42 +0200

    CompilerExtension::validateConfig() prints 'section › value' instead of section.value in exception message

--- src/DI/CompilerExtension.php
+++ src/DI/CompilerExtension.php
@@ -74,8 +74,8 @@ abstract class CompilerExtension
 		if ($extra = array_diff_key((array) $config, $expected)) {
 			$name = $name ?: $this->name;
 			$hint = Nette\Utils\ObjectHelpers::getSuggestion(array_keys($expected), key($extra));
-			$extra = $hint ? key($extra) : implode(", $name.", array_keys($extra));
-			throw new Nette\InvalidStateException("Unknown configuration option $name.$extra" . ($hint ? ", did you mean $name.$hint?" : '.'));
+			$extra = $hint ? key($extra) : implode("', '{$name} › ", array_keys($extra));
+			throw new Nette\InvalidStateException("Unknown configuration option '{$name} › {$extra}'" . ($hint ? ", did you mean '{$name} › {$hint}'?" : '.'));
 		}
 		return Config\Helpers::merge($config, $expected);
 	}
