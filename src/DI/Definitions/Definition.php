fdad4c0cc94e275d49128f16d336908cdf1fd042
David Grudl <david@grudl.com>
2018-10-11 13:29:58 +0200

    Definition: name, type, tags and autowiring moved from ServiceDefinition

--- src/DI/Definitions/Definition.php
+++ src/DI/Definitions/Definition.php
@@ -18,4 +18,186 @@ use Nette;
 abstract class Definition
 {
 	use Nette\SmartObject;
+
+	/** @var string */
+	private $name;
+
+	/** @var string|null  class or interface name */
+	private $type;
+
+	/** @var array */
+	private $tags = [];
+
+	/** @var bool|string[] */
+	private $autowired = true;
+
+	/** @var callable|null */
+	private $notifier;
+
+
+	/**
+	 * @return static
+	 * @internal  This is managed by ContainerBuilder and should not be called by user
+	 */
+	final public function setName(string $name)
+	{
+		if ($this->name) {
+			throw new Nette\InvalidStateException('Name already has been set.');
+		}
+		$this->name = $name;
+		return $this;
+	}
+
+
+	final public function getName(): ?string
+	{
+		return $this->name;
+	}
+
+
+	/**
+	 * @return static
+	 */
+	protected function setType(?string $type)
+	{
+		if ($this->notifier && $this->type !== $type) {
+			($this->notifier)();
+		}
+		if ($type === null) {
+			$this->type = null;
+		} elseif (!class_exists($type) && !interface_exists($type)) {
+			throw new Nette\InvalidArgumentException("Service '$this->name': Class or interface '$type' not found.");
+		} else {
+			$this->type = Nette\DI\Helpers::normalizeClass($type);
+		}
+		return $this;
+	}
+
+
+	final public function getType(): ?string
+	{
+		return $this->type;
+	}
+
+
+	/**
+	 * @return static
+	 */
+	final public function setTags(array $tags)
+	{
+		$this->tags = $tags;
+		return $this;
+	}
+
+
+	final public function getTags(): array
+	{
+		return $this->tags;
+	}
+
+
+	/**
+	 * @param  mixed  $attr
+	 * @return static
+	 */
+	final public function addTag(string $tag, $attr = true)
+	{
+		$this->tags[$tag] = $attr;
+		return $this;
+	}
+
+
+	/**
+	 * @return mixed
+	 */
+	final public function getTag(string $tag)
+	{
+		return $this->tags[$tag] ?? null;
+	}
+
+
+	/**
+	 * @param  bool|string|string[]  $state
+	 * @return static
+	 */
+	final public function setAutowired($state = true)
+	{
+		if ($this->notifier && $this->autowired !== $state) {
+			($this->notifier)();
+		}
+		$this->autowired = is_string($state) || is_array($state) ? (array) $state : (bool) $state;
+		return $this;
+	}
+
+
+	/**
+	 * @return bool|string[]
+	 */
+	final public function getAutowired()
+	{
+		return $this->autowired;
+	}
+
+
+	public function __clone()
+	{
+		$this->notifier = $this->name = null;
+	}
+
+
+	/********************* life cycle ****************d*g**/
+
+
+	final public function setNotifier(?callable $notifier): void
+	{
+		$this->notifier = $notifier;
+	}
+
+
+	/********************* deprecated stuff from former ServiceDefinition ****************d*g**/
+
+
+	/**
+	 * @deprecated Use setType()
+	 */
+	public function setClass(?string $type)
+	{
+		return $this->setType($type);
+	}
+
+
+	/**
+	 * @deprecated Use getType()
+	 */
+	public function getClass(): ?string
+	{
+		return $this->getType();
+	}
+
+
+	/**
+	 * @deprecated Use '$def instanceof Nette\DI\Definitions\ExternalDefinition'
+	 */
+	public function isDynamic(): bool
+	{
+		return false;
+	}
+
+
+	/**
+	 * @deprecated Use Nette\DI\Definitions\FactoryDefinition or AccessorDefinition
+	 */
+	public function getImplement(): ?string
+	{
+		return null;
+	}
+
+
+	/**
+	 * @deprecated Use getAutowired()
+	 */
+	public function isAutowired()
+	{
+		return $this->autowired;
+	}
 }
