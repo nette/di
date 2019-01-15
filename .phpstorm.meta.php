a448a259faacbaf17bbcae4e5d20da1fcc2d1958
David Grudl <david@grudl.com>
2019-01-15 19:07:10 +0700

    phpstorm.meta: supports ContainerBuilder::addDefinition

--- .phpstorm.meta.php
+++ .phpstorm.meta.php
@@ -6,3 +6,4 @@ namespace PHPSTORM_META;
 
 override(\Nette\DI\Container::getByType(0), type(0));
 override(\Nette\DI\Autowiring::getByType(0), type(0));
+override(\Nette\DI\ContainerBuilder::addDefinition(1), type(1));
