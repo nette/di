7ad57018b2222bd7620a1e6b65b812b4d460e2e4
David Grudl <david@grudl.com>
2019-12-16 00:06:05 +0100

    ContainerPanel: uses capturing

--- src/Bridges/DITracy/ContainerPanel.php
+++ src/Bridges/DITracy/ContainerPanel.php
@@ -43,10 +43,10 @@ class ContainerPanel implements Tracy\IBarPanel
 	 */
 	public function getTab(): string
 	{
-		ob_start(function () {});
-		$elapsedTime = $this->elapsedTime;
-		require __DIR__ . '/templates/ContainerPanel.tab.phtml';
-		return ob_get_clean();
+		return Nette\Utils\Helpers::capture(function () {
+			$elapsedTime = $this->elapsedTime;
+			require __DIR__ . '/templates/ContainerPanel.tab.phtml';
+		});
 	}
 
 
@@ -55,12 +55,8 @@ class ContainerPanel implements Tracy\IBarPanel
 	 */
 	public function getPanel(): string
 	{
-		$container = $this->container;
-		$rc = new \ReflectionClass($container);
-		$file = $rc->getFileName();
+		$rc = new \ReflectionClass($this->container);
 		$tags = [];
-		$instances = $this->getContainerProperty('instances');
-		$wiring = $this->getContainerProperty('wiring');
 		$types = [];
 		foreach ($rc->getMethods() as $method) {
 			if (preg_match('#^createService(.+)#', $method->getName(), $m) && $method->getReturnType()) {
@@ -75,9 +71,13 @@ class ContainerPanel implements Tracy\IBarPanel
 			}
 		}
 
-		ob_start(function () {});
-		require __DIR__ . '/templates/ContainerPanel.panel.phtml';
-		return ob_get_clean();
+		return Nette\Utils\Helpers::capture(function () use ($tags, $types, $rc) {
+			$container = $this->container;
+			$file = $rc->getFileName();
+			$instances = $this->getContainerProperty('instances');
+			$wiring = $this->getContainerProperty('wiring');
+			require __DIR__ . '/templates/ContainerPanel.panel.phtml';
+		});
 	}
 
 
