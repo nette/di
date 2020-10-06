5a5211da204290cd502ada90581f8e429b10d13c
David Grudl <david@grudl.com>
2020-10-06 16:56:17 +0200

    DIExtension: detects Tracy by presence of service @Tracy\Bar [Closes #245]

--- src/DI/Extensions/DIExtension.php
+++ src/DI/Extensions/DIExtension.php
@@ -36,7 +36,7 @@ final class DIExtension extends Nette\DI\CompilerExtension
 		$this->time = microtime(true);
 
 		$this->config = new class {
-			/** @var bool */
+			/** @var ?bool */
 			public $debugger;
 			/** @var string[] */
 			public $excluded = [];
@@ -53,7 +53,6 @@ final class DIExtension extends Nette\DI\CompilerExtension
 			/** @var string[]|bool|null */
 			public $types = true;
 		};
-		$this->config->debugger = interface_exists(\Tracy\IBarPanel::class);
 	}
 
 
@@ -81,7 +80,10 @@ final class DIExtension extends Nette\DI\CompilerExtension
 		$this->restrictTags($class);
 		$this->restrictTypes($class);
 
-		if ($this->debugMode && $this->config->debugger) {
+		if (
+			$this->debugMode &&
+			($this->config->debugger ?? $this->getContainerBuilder()->getByType(\Tracy\Bar::class))
+		) {
 			$this->enableTracyIntegration();
 		}
 
