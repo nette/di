254fff9bebf9337dab49792a4ba35588484995bd
Rick Strafy <45132928+Rixafy@users.noreply.github.com>
2021-09-11 02:01:58 +0200

    Add typehints to Container::getByType objects (master)

--- src/DI/Container.php
+++ src/DI/Container.php
@@ -218,6 +218,10 @@ class Container
 
 	/**
 	 * Resolves service by type.
+	 * @template T
+	 * @param  class-string<T>  $type  fully qualified class name
+	 * @param  bool  $throw  exception if service doesn't exist?
+	 * @return T|null  service
 	 * @throws MissingServiceException
 	 */
 	public function getByType(string $type, bool $throw = true): ?object
