113f2f326aadeecced504c78715abf2a0a4cb6df
David Grudl <david@grudl.com>
2021-09-20 00:34:16 +0200

    used PHP 8 features

--- src/DI/Definitions/Reference.php
+++ src/DI/Definitions/Reference.php
@@ -26,7 +26,7 @@ final class Reference
 
 	public static function fromType(string $value): static
 	{
-		if (strpos($value, '\\') === false) {
+		if (!str_contains($value, '\\')) {
 			$value = '\\' . $value;
 		}
 		return new static($value);
@@ -47,13 +47,13 @@ final class Reference
 
 	public function isName(): bool
 	{
-		return strpos($this->value, '\\') === false && $this->value !== self::SELF;
+		return !str_contains($this->value, '\\') && $this->value !== self::SELF;
 	}
 
 
 	public function isType(): bool
 	{
-		return strpos($this->value, '\\') !== false;
+		return str_contains($this->value, '\\');
 	}
 
 
