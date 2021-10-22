6a0bdc6826d3fcbc3ebcd45ebacfc07e3183ee0d
Markus Staab <maggus.staab@googlemail.com>
2021-10-22 12:19:14 +0200

    ContainerLoader: make sure file-muted is released when exceptions are thrown

--- src/DI/ContainerLoader.php
+++ src/DI/ContainerLoader.php
@@ -64,28 +64,31 @@ class ContainerLoader
 		} elseif (!@flock($handle, LOCK_EX)) { // @ is escalated to exception
 			throw new Nette\IOException(sprintf("Unable to acquire exclusive lock on '%s.lock'. %s", $file, Nette\Utils\Helpers::getLastError()));
 		}
+		
+		try {
+			if (!is_file($file) || $this->isExpired($file, $updatedMeta)) {
+				if (isset($updatedMeta)) {
+					$toWrite["$file.meta"] = $updatedMeta;
+				} else {
+					[$toWrite[$file], $toWrite["$file.meta"]] = $this->generate($class, $generator);
+				}
 
-		if (!is_file($file) || $this->isExpired($file, $updatedMeta)) {
-			if (isset($updatedMeta)) {
-				$toWrite["$file.meta"] = $updatedMeta;
-			} else {
-				[$toWrite[$file], $toWrite["$file.meta"]] = $this->generate($class, $generator);
-			}
-
-			foreach ($toWrite as $name => $content) {
-				if (file_put_contents("$name.tmp", $content) !== strlen($content) || !rename("$name.tmp", $name)) {
-					@unlink("$name.tmp"); // @ - file may not exist
-					throw new Nette\IOException(sprintf("Unable to create file '%s'.", $name));
-				} elseif (function_exists('opcache_invalidate')) {
-					@opcache_invalidate($name, true); // @ can be restricted
+				foreach ($toWrite as $name => $content) {
+					if (file_put_contents("$name.tmp", $content) !== strlen($content) || !rename("$name.tmp", $name)) {
+						@unlink("$name.tmp"); // @ - file may not exist
+						throw new Nette\IOException(sprintf("Unable to create file '%s'.", $name));
+					} elseif (function_exists('opcache_invalidate')) {
+						@opcache_invalidate($name, true); // @ can be restricted
+					}
 				}
 			}
-		}
 
-		if ((@include $file) === false) { // @ - error escalated to exception
-			throw new Nette\IOException(sprintf("Unable to include '%s'.", $file));
+			if ((@include $file) === false) { // @ - error escalated to exception
+				throw new Nette\IOException(sprintf("Unable to include '%s'.", $file));
+			}
+		} finally {
+			flock($handle, LOCK_UN);
 		}
-		flock($handle, LOCK_UN);
 	}
 
 
