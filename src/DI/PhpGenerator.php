689a43cc80e496a681fc05867e9ca41cf83bcbf1
David Grudl <david@grudl.com>
2021-11-23 18:10:31 +0100

    PhpGenerator: do not format statement twice or incorrect

--- src/DI/PhpGenerator.php
+++ src/DI/PhpGenerator.php
@@ -122,7 +122,9 @@ declare(strict_types=1);
 				return $this->formatPhp($entity, $arguments);
 
 			case is_string($entity): // create class
-				return $this->formatPhp("new $entity" . ($arguments ? '(...?)' : ''), $arguments ? [$arguments] : []);
+				return $arguments
+					? $this->formatPhp("new $entity(...?)", [$arguments])
+					: $this->formatPhp("new $entity", []);
 
 			case is_array($entity):
 				switch (true) {
@@ -133,9 +135,9 @@ declare(strict_types=1);
 						}
 						$prop = $entity[0] instanceof Reference
 							? $this->formatPhp('?->?', [$entity[0], $name])
-							: $this->formatPhp($entity[0] . '::$?', [$name]);
+							: $this->formatPhp('?::$?', [$entity[0], $name]);
 						return $arguments
-							? $this->formatPhp($prop . ($append ? '[]' : '') . ' = ?', [$arguments[0]])
+							? $this->formatPhp(($append ? '?[]' : '?') . ' = ?', [new Php\Literal($prop), $arguments[0]])
 							: $prop;
 
 					case $entity[0] instanceof Statement:
@@ -143,16 +145,16 @@ declare(strict_types=1);
 						if (substr($inner, 0, 4) === 'new ') {
 							$inner = "($inner)";
 						}
-						return $this->formatPhp("$inner->?(...?)", [$entity[1], $arguments]);
+						return $this->formatPhp('?->?(...?)', [new Php\Literal($inner), $entity[1], $arguments]);
 
 					case $entity[0] instanceof Reference:
 						return $this->formatPhp('?->?(...?)', [$entity[0], $entity[1], $arguments]);
 
 					case $entity[0] === '': // function call
-						return $this->formatPhp("$entity[1](...?)", [$arguments]);
+						return $this->formatPhp('?(...?)', [new Php\Literal($entity[1]), $arguments]);
 
 					case is_string($entity[0]): // static method call
-						return $this->formatPhp("$entity[0]::$entity[1](...?)", [$arguments]);
+						return $this->formatPhp('?::?(...?)', [new Php\Literal($entity[0]), $entity[1], $arguments]);
 				}
 		}
 
