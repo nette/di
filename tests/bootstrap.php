e44f34222193caa7000379c8d3c2193fb2a45a06
David Grudl <david@grudl.com>
2021-12-15 16:58:31 +0100

    added PHP 8 typehints

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -63,7 +63,7 @@ class Notes
 }
 
 
-function createContainer($source, $config = null, $params = []): ?Nette\DI\Container
+function createContainer($source, $config = null, array $params = []): ?Nette\DI\Container
 {
 	$class = 'Container' . md5((string) lcg_value());
 	if ($source instanceof Nette\DI\ContainerBuilder) {
