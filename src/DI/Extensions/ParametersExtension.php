5d8d565fd15735c6963bfcf1f59475261df891e4
David Grudl <david@grudl.com>
2023-08-01 15:05:30 +0200

    ParametersExtension: dynamic parameters are evaluated immediately [Closes #291]

--- src/DI/Extensions/ParametersExtension.php
+++ src/DI/Extensions/ParametersExtension.php
@@ -60,7 +60,7 @@ final class ParametersExtension extends Nette\DI\CompilerExtension
 	{
 		$params = $this->getContainerBuilder()->parameters;
 		array_walk_recursive($params, function (&$val): void {
-			if ($val instanceof Nette\DI\Definitions\Statement || $val instanceof DynamicParameter) {
+			if ($val instanceof Nette\DI\Definitions\Statement) {
 				$val = null;
 			}
 		});
