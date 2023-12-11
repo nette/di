722be3701213f1558a18db044e79446e03ae2d18
David Grudl <david@grudl.com>
2023-12-11 17:12:33 +0100

    removed support for three ... dots

--- src/DI/Config/Adapters/NeonAdapter.php
+++ src/DI/Config/Adapters/NeonAdapter.php
@@ -216,15 +216,6 @@ final class NeonAdapter implements Nette\DI\Config\Adapter
 			if ($attr->value instanceof Node\LiteralNode && $attr->value->value === '_') {
 				unset($node->attributes[$i]);
 				$index = true;
-
-			} elseif (
-				$attr->value instanceof Node\LiteralNode
-				&& $attr->value->value === '...'
-				&& !$this->isFirstClassCallable($node)
-			) {
-				trigger_error("Replace ... with _ in configuration file '$this->file'.", E_USER_DEPRECATED);
-				unset($node->attributes[$i]);
-				$index = true;
 			}
 		}
 	}
