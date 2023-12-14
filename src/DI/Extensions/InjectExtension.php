e9b9331eb34846b36c44a25728eee4e15a90b40c
David Grudl <david@grudl.com>
2023-12-14 13:48:45 +0100

    annotations @var are no longer supported (BC break)

--- src/DI/Extensions/InjectExtension.php
+++ src/DI/Extensions/InjectExtension.php
@@ -117,19 +117,15 @@ final class InjectExtension extends DI\CompilerExtension
 	{
 		$res = [];
 		foreach ((new \ReflectionClass($class))->getProperties() as $rp) {
-			$hasAttr = $rp->getAttributes(DI\Attributes\Inject::class);
-			if ($hasAttr || DI\Helpers::parseAnnotation($rp, 'inject') !== null) {
+			if (
+				$rp->getAttributes(DI\Attributes\Inject::class)
+				|| DI\Helpers::parseAnnotation($rp, 'inject') !== null
+			) {
 				if (!$rp->isPublic() || $rp->isStatic() || $rp->isReadOnly()) {
 					throw new Nette\InvalidStateException(sprintf('Property %s for injection must not be static, readonly and must be public.', Reflection::toString($rp)));
 				}
 
-				$type = Nette\Utils\Type::fromReflection($rp);
-				if (!$type && !$hasAttr && ($annotation = DI\Helpers::parseAnnotation($rp, 'var'))) {
-					$annotation = Reflection::expandClassName($annotation, Reflection::getPropertyDeclaringClass($rp));
-					$type = Nette\Utils\Type::fromString($annotation);
-				}
-
-				$res[$rp->getName()] = DI\Helpers::ensureClassType($type, 'type of property ' . Reflection::toString($rp));
+				$res[$rp->getName()] = DI\Helpers::ensureClassType(Nette\Utils\Type::fromReflection($rp), 'type of property ' . Reflection::toString($rp));
 			}
 		}
 
