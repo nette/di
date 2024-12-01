de52bfc22ed0c16759cc47da1d9f3e4d94324b3b
David Grudl <david@grudl.com>
2024-12-01 22:15:47 +0100

    removed Definition::generateMethod() (BC break)

--- src/DI/Definitions/Definition.php
+++ src/DI/Definitions/Definition.php
@@ -171,7 +171,7 @@ abstract class Definition
 	abstract public function complete(Nette\DI\Resolver $resolver): void;
 
 
-	//abstract public function generateCode(Nette\DI\PhpGenerator $generator): string;
+	abstract public function generateCode(Nette\DI\PhpGenerator $generator): string;
 
 
 	final public function setNotifier(?\Closure $notifier): void
@@ -181,13 +181,6 @@ abstract class Definition
 
 
 	/********************* deprecated stuff from former ServiceDefinition ****************d*g**/
-
-
-	/** @deprecated */
-	public function generateMethod(Nette\PhpGenerator\Method $method, Nette\DI\PhpGenerator $generator): void
-	{
-		$method->setBody($this->generateCode($generator));
-	}
 
 
 	/** @deprecated Use setType() */
