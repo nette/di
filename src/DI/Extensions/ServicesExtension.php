b95b0b05a1805164f7d8d86a8b7a723f5de7e4e4
David Grudl <david@grudl.com>
2024-12-02 05:53:18 +0100

    exception messages use [Service ...]\n format [WIP]
    added Definition::getDescriptor(), Helpers::entityToString()

--- src/DI/Extensions/ServicesExtension.php
+++ src/DI/Extensions/ServicesExtension.php
@@ -54,7 +54,7 @@ final class ServicesExtension extends Nette\DI\CompilerExtension
 				$this->getContainerBuilder()->removeDefinition($name);
 				return;
 			} elseif (!empty($config->alteration) && !$this->getContainerBuilder()->hasDefinition($name)) {
-				throw new Nette\DI\InvalidConfigurationException('missing original definition for alteration.');
+				throw new Nette\DI\InvalidConfigurationException('Missing original definition for alteration.');
 			}
 
 			$def = $this->retrieveDefinition($name, $config);
@@ -69,7 +69,12 @@ final class ServicesExtension extends Nette\DI\CompilerExtension
 			$this->{$methods[$config->defType]}($def, $config);
 			$this->updateDefinition($def, $config);
 		} catch (\Throwable $e) {
-			throw new Nette\DI\InvalidConfigurationException(($name ? "Service '$name': " : '') . $e->getMessage(), 0, $e);
+			$message = $e->getMessage();
+			if ($name && !str_starts_with($message, '[Service ')) {
+				$message = "[Service '$name']\n$message";
+			}
+
+			throw new Nette\DI\InvalidConfigurationException($message, 0, $e);
 		}
 	}
 
